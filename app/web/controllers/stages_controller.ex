3917fc836d7f402ab93515dd132621489ef9dfe3
Artur Roszczyk <artur.roszczyk@gmail.com>
2015-04-18 19:22:24 +0200

    [Backend] create stage

--- app/web/controllers/stages_controller.ex
+++ app/web/controllers/stages_controller.ex
@@ -8,4 +8,14 @@ defmodule Crm.StagesController do
     stages = Repo.all(Stage)
     render conn, stages: stages
   end
+
+  def create(conn, params) do
+    stage = Map.merge(%Stage{}, atomize_keys(params["stage"]))
+    stage = Repo.insert(stage)
+    render conn, stage: stage
+  end
+
+  defp atomize_keys(struct) do
+    Enum.reduce struct, %{}, fn({k, v}, map) -> Map.put(map, String.to_atom(k), v) end
+  end
 end
--- app/web/views/stages_view.ex
+++ app/web/views/stages_view.ex
@@ -4,4 +4,15 @@ defmodule Crm.StagesView do
   def render("index.json", %{stages: stages}) do
     %{stages: stages}
   end
+
+
+  def render("create.json", %{stage: stage}) do
+    render("show.json", %{stage: stage})
+  end
+
+
+  def render("show.json", %{stage: stage}) do
+    %{stage: stage}
+  end
+
 end
