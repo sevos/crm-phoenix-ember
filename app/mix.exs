e62597c886746951ffcceb224ba9f4b882f88c83
Artur Roszczyk <artur.roszczyk@gmail.com>
2015-04-18 20:06:28 +0200

    Ignore rel

--- app/mix.exs
+++ app/mix.exs
@@ -32,6 +32,7 @@ defmodule Crm.Mixfile do
      {:phoenix_ecto, "~> 0.3"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_live_reload, "~> 0.3"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:exrm, "~> 0.15.3"}]
   end
 end
